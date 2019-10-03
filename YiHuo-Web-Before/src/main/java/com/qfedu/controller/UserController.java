fc911a539d3567f8f0248d3824244671bd187624
jiao1260095730 <1260095730@qq.com>
2019-10-03 11:27:57 +0800

    焦斌-第四次上传 注册页面，生成验证码，验证成功进入登录（小修改）

--- YiHuo-Web-Before/src/main/java/com/qfedu/controller/UserController.java
+++ YiHuo-Web-Before/src/main/java/com/qfedu/controller/UserController.java
@@ -2,6 +2,8 @@ package com.qfedu.controller;
 
 import com.qfedu.entry.User;
 import com.qfedu.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
+@Api(tags = "该类用于后台操作")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @ApiOperation(value = "该方法是用户注册时发送验证码并保存至数据库")
     @RequestMapping("/validate")
     @ResponseBody
     public String validate(String email) {
@@ -24,7 +28,9 @@ public class UserController {
         return "fail";
     }
 
+    @ApiOperation(value =  "该方法用于用户使用邮箱注册，输入邮箱，密码，验证码")
     @RequestMapping("/register")
+    @ResponseBody
     public String register(User user) {
         int count = userService.register(user);
 
@@ -34,8 +40,4 @@ public class UserController {
         return "fail";
     }
 
-    @RequestMapping("/login")
-    public String login(User user) {
-        return "success";
-    }
 }
