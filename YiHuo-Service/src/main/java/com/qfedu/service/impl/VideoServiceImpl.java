59f5ed09981d256fb4d7c08eddd3728d3bb586e3
jiao1260095730 <1260095730@qq.com>
2019-10-03 17:33:28 +0800

    焦斌-提交第10次，添加video展示功能，查询数据库，展示视频列表

--- YiHuo-Service/src/main/java/com/qfedu/service/impl/VideoServiceImpl.java
+++ YiHuo-Service/src/main/java/com/qfedu/service/impl/VideoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.qfedu.service.impl;
 
+import com.qfedu.entry.Video;
 import com.qfedu.mapper.VideoMapper;
 import com.qfedu.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VideoServiceImpl implements VideoService {
@@ -12,4 +15,7 @@ public class VideoServiceImpl implements VideoService {
     VideoMapper videoMapper;
 
 
+    public List<Video> selectAllVideo() {
+        return videoMapper.selectAllVideo();
+    }
 }
