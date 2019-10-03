813421453298e1335e8e29abb6521df2a524dea4
hetingwei <1844167725@qq.com>
2019-10-03 17:10:24 +0800

    修改User实体类，增加完善资料页面的实体

--- YiHuo-Entry/src/main/java/com/qfedu/entry/User.java
+++ YiHuo-Entry/src/main/java/com/qfedu/entry/User.java
@@ -3,19 +3,63 @@ package com.qfedu.entry;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.xml.soap.SAAJResult;
+
 @Getter
 @Setter
 public class User {
 
     private int id;
-    private String userName;
     private String email;
+    /**
+     * 昵称
+     */
+    private String userName;
+    private String password;
     private Boolean gender;
     private String phone;
     private String headImgUrl;
-    private String bgImgUrl;
-    private String password;
+    /**
+     * 信息
+     */
     private String info;
+    /**
+     * 背景图片
+     */
+    private String bgImgUrl;
     private String validateNum;
-
+    /**
+     * 获赞数量
+     */
+    private int huoZan;
+    /**
+     * 粉丝
+     */
+    private int fans;
+    private int guanZhuNum;
+    private String vip;
+    /**
+     * 生日
+     */
+    private String birthday;
+    /**
+     * 地区
+     */
+    private String adress;
+    /**
+     * 职业
+     */
+    private String profession;
+    /**
+     * 行业
+     */
+    private String trade;
+    /**
+     * 学历
+     */
+    private String eduction;
+    /**
+     * 购买课程ID
+     */
+    private int buyCourseId;
 }
