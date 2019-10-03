72a6966eef1557e86ec2954ce1c646609d698f99
jiao1260095730 <1260095730@qq.com>
2019-10-03 14:50:30 +0800

    焦斌-注册功能第五次上传 注册页面，生成验证码，验证成功进入登录

---
+++ YiHuo-Web-Before/src/main/java/com/qfedu/controller/MySwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.qfedu.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration      //让Spring来加载该类配置
+@EnableSwagger2     //启用Swagger2
+public class MySwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.qfedu.controller")) //扫描的包
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("老闫项目接口API 文档")
+                .description("HTTP对外开放接口")
+                .version("1.0.0")
+                .termsOfServiceUrl("http://xxx.xxx.com")
+                .license("LICENSE")
+                .licenseUrl("http://xxx.xxx.com")
+                .build();
+    }
+}
--- YiHuo-Web-Before/src/main/java/com/qfedu/controller/UserController.java
+++ YiHuo-Web-Before/src/main/java/com/qfedu/controller/UserController.java
@@ -3,10 +3,13 @@ package com.qfedu.controller;
 import com.qfedu.entry.User;
 import com.qfedu.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,7 +21,7 @@ public class UserController {
     UserService userService;
 
     @ApiOperation(value = "该方法是用户注册时发送验证码并保存至数据库")
-    @RequestMapping("/validate")
+    @RequestMapping(value = "/validate", method = RequestMethod.POST)
     @ResponseBody
     public String validate(String email) {
         int count = userService.validate(email);
@@ -29,8 +32,12 @@ public class UserController {
     }
 
     @ApiOperation(value =  "该方法用于用户使用邮箱注册，输入邮箱，密码，验证码")
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
+    @ApiImplicitParams(
+            {@ApiImplicitParam(name = "email", value = "邮箱", required = true, dataType = "String"),
+             @ApiImplicitParam(name = "password", value = "密码", required = true, dataType = "String")}
+    )
     public String register(User user) {
         int count = userService.register(user);
 
