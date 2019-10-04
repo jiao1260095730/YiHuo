a47095ff117e8eedfaef66d725cd9da6f6e5a723
jiao1260095730 <1260095730@qq.com>
2019-10-04 09:42:37 +0800

    焦斌-提交第11次，修改video控制层部分注解问题

--- YiHuo-Web-Before/src/main/java/com/qfedu/controller/VideoController.java
+++ YiHuo-Web-Before/src/main/java/com/qfedu/controller/VideoController.java
@@ -2,22 +2,27 @@ package com.qfedu.controller;
 
 import com.qfedu.entry.Video;
 import com.qfedu.service.VideoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/video")
+@Api(tags = "该类实现所有关于video的功能")
 public class VideoController {
 
     @Autowired
     VideoService videoService;
 
 
-    @RequestMapping("/showList")
+    @RequestMapping(value = "/showList",method = RequestMethod.POST)
+    @ApiOperation(value = "该方法用来展示视频列表")
     public String videoList(Model model) {
         List<Video> videoList = videoService.selectAllVideo();
         model.addAttribute("videoList", videoList);
